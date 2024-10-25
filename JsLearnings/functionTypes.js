2696f611d60044e9ebfa8b7a7f471fd5c0b1a44f
vidya1926 <83778108+vidya1926@users.noreply.github.com>
2024-10-25 23:39:58 +0530

    Update functionTypes.js

--- JsLearnings/functionTypes.js
+++ JsLearnings/functionTypes.js
@@ -1,4 +1,42 @@
 
+//named fnction or function declaration
+
+function getBrowserVersion(bVersion){
+    console.log("browserVersion")
+    return bVersion
+}
+let response=getBrowserVersion(112)
+
+
+//Function expression
+let browser=function getBrowserrName(bName){
+    return bName + response
+}
+console.log(browser("Chrome"))
+
+//anonymous function
+let user=function (username){
+   return username
+}
+console.log(user("DemoUser"))
+
+//Arrow Function
+let userCredentials =()=>console.log("username as Vidya, password as 123")
+userCredentials()
+
+//IIFE
+console.log(((a,b)=>a+b)(2,3));
+
+//Higher order 
+/*
+ callback
+ promise
+ async 
+ await
+*/
+
+
+
 let pizzaType;
 function orderPizza(){
      console.log("Online pizza")
